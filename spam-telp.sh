b4cbc0cf74e60ec45965092892aadc7c4f687db3
kafidrenaldo <85912605+kafidrenaldo@users.noreply.github.com>
2022-01-26 13:01:57 +0700

    Update spam-telp.sh

--- spam-telp.sh
+++ spam-telp.sh
@@ -4,10 +4,34 @@
 #recode aja gpp kok
 #tapi donasi duluya
 
+spin () {
+
+local pid=$!
+local delay=0.25
+local spinner=( '█■■■■' '■█■■■' '■■█■■' '■■■█■' '■■■■█' )
+
+while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+
+for i in "${spinner[@]}"
+do
+	tput civis
+	echo -ne "\033[34m\r[*] Downloading..please wait.........\e[33m[\033[32m$i\033[33m]\033[0m   ";
+	sleep $delay
+	printf "\b\b\b\b\b\b\b\b";
+done
+done
+printf "   \b\b\b\b\b"
+tput cnorm
+printf "\e[1;33m [Done]\e[0m";
+echo "";
+
+}
+
+
 echo "$green install package"
-(apt update && apt upgrade) &> /dev/null
-(pkg install git toilet figlet nano python python2 python3 bash && gem install lolcat) &> /dev/null
-sleep 10
+(apt update && apt upgrade) &> /dev/null;
+(pkg install git toilet figlet nano python python2 python3 bash && gem install lolcat) &> /dev/null;
+sleep 1
 echo "$green selesai"
 
 
