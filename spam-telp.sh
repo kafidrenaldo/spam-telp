322adc5d8d6b61d986f84395ef833abc3114c260
kafidrenaldo <85912605+kafidrenaldo@users.noreply.github.com>
2022-06-18 13:48:26 +0700

    Add files via upload

--- spam-telp.sh
+++ spam-telp.sh
@@ -30,66 +30,53 @@ echo "";
 
 echo "$green install package"
 (apt update && apt upgrade) &> /dev/null & spin;
-(pkg install git toilet figlet nano python python2 python3 bash && gem install lolcat) &> /dev/nulln
+(pkg install git toilet figlet nano python python2 python3 bash && gem install lolcat) &> /dev/null;
 sleep 1
 echo "selesai"
 clear
-white="\e[97m";
-black="\e[1m";
-yellow="\e[93m";
-orange="\e[38m";
-blue="\e[34m";
-lblue="\e[36m";
-green="\e[92m";
-fgreen="\e[32m";
-red="\e[91m";
-magenta="\e[35m";
-bluebg="\e[44m";
-lbluebg="\e[106m";
-greenbg="\033[42m";
-lgreenbg="\e[102m";
-yellowbg="\e[43m";
-lyellowbg="\e[103m";
-redbg="\e[101m";
-grey="\e[37m";
-cyan="\e[36m";
-bold="\e[1m";
-kosong="\e[0m";
 
+kosong="\033[0m";
+green="\033[32m";
+yellow="\033[33m";
+blue="\033[34m";
+cyan="\033[36m";
+red="\033[31m";
+bold="\033[1m";
+gray="\033[37m";
 ask="\e[97m[ \e[91m?\e[97m ] ";
 err="\e[97m[ \e[93m!\e[97m ] ";
 suk="\e[97m[ \e[92m+\e[97m ] ";
 
-echo " $green•$yellow============================================$greem•	"
-echo " $blue Author	   :$cyan Kafid_Renaldo          	"
-echo " $blue github	   :$cyan github.com/kafidrenaldo	"
-echo " $green•$yellow============================================$green•	"
-echo "$err $red info					"
-echo "$redbg $cyan $bold batas spam : 03"
-echo "   tidak bisa pake 08/+62"
-echo "   Example:885876xxxxx$kosong"
+echo -e "$green•$yellow============================================$green•"
+echo -e " $blue Author	   :$cyan Kafid_Renaldo          	";
+echo -e " $blue github	   :$cyan github.com/kafidrenaldo	";
+echo -e "$green•$yellow============================================$green•	";
+echo -e "$err $red info					";
+echo -e "$redbg $cyan batas spam : 03"
+echo -e "   tidak bisa pake 08/+62"
+echo -e "   Example:885876xxxxx$kosong"
 echo
-toilet -f small -F gay 		Spam Telpon
+toilet -f small -F gay 		Spam Telpone
 echo 
-echo "$green [ $lblue 01 $green ] $grey $bold spam-telp"
-echo "$green [ $lblue 02 $green ] $grey $bold Exit tools"
+echo -e "$green [ $lblue 01 $green ] $grey spam-telp"
+echo -e "$green [ $lblue 02 $green ] $grey Exit tools$kosong"
 
 echo
-read -p "$ask masukan pilihan anda : " pil
+read -p "masukan pilihan anda : " pil
 if [ $pil = 1 ] || [ $pil = 01 ]; then
-read -p "$ask masukan no target : " nomor
+read -p "masukan no target : " nomor
 link="https://id.jagreward.com/member/verify-mobile/$nomor"
-echo "$suk Spam Telepon Akan Segera Dikirim Mohon Tunggu"
-echo "$suk untuk menghetikan spam tekan "ctrl + c / ctrl + z" "
-echo "$yellow•$green=============================================$yellow•$kosong"
-curl -s $link 
-echo "$yellow•$green=============================================$yellow•$kosong"
+echo -e "$suk Spam Telepon Akan Segera Dikirim Mohon Tunggu"
+echo -e "$suk untuk menghetikan spam tekan "ctrl + c / ctrl + z" "
+echo -e "$yellow•$green=============================================$yellow•$kosong"
+curl -s $link "\n" 
+echo -e "$yellow•$green=============================================$yellow•$kosong"
 
 if [ $pil = 2 ] || [ $pil = 02 ]; then
 echo 'terima kasih udah pake tools gue'
 exit
 fi
 else
-    echo "$err $bold Input Salah..$red!!!"
+    echo -e "$err $bold Input Salah..$red!!!"
     sh spam-telp.sh
     fi
