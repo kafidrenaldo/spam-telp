227b1a5e5e8600d034a7046b6c6d84820774f9a2
kafidrenaldo <85912605+kafidrenaldo@users.noreply.github.com>
2022-06-22 05:33:03 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#mau ngapain om
+#mau recode y
+#recode aja gpp kok
+#tapi donasi duluya
+
+spin () {
+
+local pid=$!
+local delay=0.25
+local spinner=( '█■■■■' '■█■■■' '■■█■■' '■■■█■' '■■■■█' )
+
+while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+
+for i in "${spinner[@]}"
+do
+	tput civis
+	echo -ne "\033[34m\r[*] Downloading..please wait.........\e[33m[\033[32m$i\033[33m]\033[0m   ";
+	sleep $delay
+	printf "\b\b\b\b\b\b\b\b";
+done
+done
+printf "   \b\b\b\b\b"
+tput cnorm
+printf "\e[1;33m [Done]\e[0m";
+echo "";
+
+}
+
+chmod +x .spam
+echo -e $err"install package"
+(apt update && apt upgrade) &> /dev/null;
+(pkg install git toilet figlet nano python python2 python3 bash && gem install lolcat) &> /dev/null & spin;
+echo "selesai"
+slepp 1
+clear
+
+./spam
+#if check = true
+#echo gas spam
